d4b8a97d0b00c3f2e3adfd717d41f290ee9f16a5
Jumbix <raulprenciu@yahoo.com>
2020-10-05 14:56:09 +0300

    bindingf

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -31,18 +31,92 @@ namespace WpfApp2
 
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void CarName_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(carName.SelectedItem == "Audi")
+            if (carName.SelectedItem.ToString() == "Audi")
             {
-
+                var t = new ComboBoxItem();
+                t.Content = "A1";
+                carModel.Items.Add(t);
+                t.Content = "A2";
+                carModel.Items.Add(t);
+                t.Content = "A3";
+                carModel.Items.Add(t);
+                t.Content = "A4";
+                carModel.Items.Add(t);
+                t.Content = "A5";
+                carModel.Items.Add(t);
+                t.Content = "A6";
+                carModel.Items.Add(t);
+                t.Content = "A7";
+                carModel.Items.Add(t);
+                t.Content = "A8";
+                carModel.Items.Add(t);
+            }
+            if (carName.SelectedItem.ToString() == "BMW")
+            {
+                var t = new ComboBoxItem();
+                t.Content = "Seria1";
+                carModel.Items.Add(t);
+                t.Content = "Seria2";
+                carModel.Items.Add(t);
+                t.Content = "Seria3";
+                carModel.Items.Add(t);
+                t.Content = "Seria4";
+                carModel.Items.Add(t);
+                t.Content = "Seria5";
+                carModel.Items.Add(t);
+            }
+            if (carName.SelectedItem.ToString() == "Chevrolette")
+            {
+                var t = new ComboBoxItem();
+                t.Content = "Camaro";
+                carModel.Items.Add(t);
+                t.Content = "Dahoa";
+                carModel.Items.Add(t);
+                t.Content = "Corvette";
+                carModel.Items.Add(t);
+            }
+            if (carName.SelectedItem.ToString() == "Dacia")
+            {
+                var t = new ComboBoxItem();
+                t.Content = "1900";
+                carModel.Items.Add(t);
+                t.Content = "Logan";
+                carModel.Items.Add(t);
+                t.Content = "Nova";
+                carModel.Items.Add(t);
+                t.Content = "GIFonu";
+                carModel.Items.Add(t);
+                t.Content = "Duster";
+                carModel.Items.Add(t);
+                t.Content = "Papuc";
+                carModel.Items.Add(t);
+                t.Content = "Stepway";
+                carModel.Items.Add(t);
+            }
+            if (carName.SelectedItem.ToString() == "Ford")
+            {
+                var t = new ComboBoxItem();
+                t.Content = "Fiesta";
+                carModel.Items.Add(t);
+                t.Content = "Focus";
+                carModel.Items.Add(t);
+                t.Content = "Mustang";
+                carModel.Items.Add(t);
+            }
+            if (carName.SelectedItem.ToString() == "Mercedes-Benz")
+            {
+                var t = new ComboBoxItem();
+                t.Content = "Camaro";
+                carModel.Items.Add(t);
+                t.Content = "Cabriolette";
+                carModel.Items.Add(t);
+                t.Content = "Corvette";
+                carModel.Items.Add(t);
             }
         }
 
-        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -60,6 +134,21 @@ namespace WpfApp2
         }
 
         private void CarModel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void PriceRange_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
 
         }
